25091df103bdf9c259345449d53b41a4c3222622
imranalizue-rgb <imranali.zue@gmail.com>
2026-07-08 13:44:44 -0400

    changed base path to relative

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()], // <--- Is line ke aakhir mein comma (,) lagayein
-  base: '/my-website/', 
+  plugins: [react(), tailwindcss()],
+  base: './',
 })
